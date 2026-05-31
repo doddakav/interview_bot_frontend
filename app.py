9cb6b463f9e5b9ede6dd247776ad2c84e38fa250
vinod <vinod.doddaka@gmail.com>
2026-05-31 18:00:56 +0530

    inteview bot using streamlit and render with groq api key  frontend

---
+++ app.py
@@ -0,0 +1,49 @@
+import streamlit as st
+import requests
+
+API_URL = "http://127.0.0.1:8000"
+
+st.title("🤖 AI Interview Bot")
+
+with st.form("Chatbot"):
+
+    topic = st.text_input(
+        "Enter Topic"
+    )
+
+    level = st.selectbox(
+        "Difficulty Level",
+        ["Easy", "Medium", "Advanced"]
+    )
+
+    ways = st.multiselect(
+        "Question Type",
+        ["MCQ", "Theory", "Coding"]
+    )
+
+    submit = st.form_submit_button(
+        "Generate Questions"
+    )
+
+if submit:
+
+    response = requests.post(
+        f"{API_URL}/generate-question",
+        json={
+            "topic": topic,
+            "level": level,
+            "ways": ways
+        }
+    )
+
+    if response.status_code == 200:
+
+        questions = response.json()["questions"]
+
+        st.subheader("Generated Questions")
+
+        st.write(questions)
+
+    else:
+
+        st.error("Unable to connect to backend")
